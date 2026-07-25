571dec94e7161c277ee5e4e8701a113cc426c7e4
Michdriod <oluwasegunmichealalejo@gmail.com>
2026-07-26 00:38:37 +0100

    fix for mobile1.2

--- src/components/LogoGrid.tsx
+++ src/components/LogoGrid.tsx
@@ -1,22 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import { clients } from "@/data/clients";
+import { StaggeredWrapper } from "@/components/StaggeredWrapper";
 
 export function LogoGrid() {
   return (
     <div className="mx-auto max-w-5xl">
       <div className="flex flex-wrap items-center justify-center gap-8">
-        {clients.map((client) => (
-          <div
-            key={client.name}
-            className="relative flex h-14 w-28 items-center justify-center"
-          >
-            <Image
-              src={client.logo}
-              alt={`${client.name} logo`}
-              fill
-              className="object-contain"
-            />
-          </div>
+        {clients.map((client, i) => (
+          <StaggeredWrapper key={client.name} delay={i * 0.08}>
+            <div className="relative flex h-14 w-28 items-center justify-center">
+              <Image
+                src={client.logo}
+                alt={`${client.name} logo`}
+                fill
+                className="object-contain"
+              />
+            </div>
+          </StaggeredWrapper>
         ))}
       </div>
     </div>
